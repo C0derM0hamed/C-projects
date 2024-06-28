e55795fb8826b6b931cc8ce07f1780383ca1af70
C0derM0hamed <mohamedmostafa2011360@gmail.com>
2024-06-28 22:54:39 +0300

    this is first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oob1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            point p1 = new point(1,2,3);
+            point p2 = new point(4, 5, 6);
+            p1.DispalyPoint();
+            Console.WriteLine(point.CalculateDistatice(p1,p2));
+        }
+    }
+}
---
+++ point.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oob1
+{
+    internal class point
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+        public int z { get; set; }
+
+        public point(int x, int y, int z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+        public void DispalyPoint()
+        {
+            Console.Write($"x : {x},y : {y}, z : {z} ");
+        }
+        public static int CalculateDistatice(point p1,point p2)
+        {
+            return Math.Abs(p1.x - p2.x);
+        } 
+    }
+}
